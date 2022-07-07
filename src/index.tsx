b98d60a98d2fc313c7843f18d378f0ffa6add20b
Sam Huynh (he/him) <samhwang2112.dev@gmail.com>
2022-07-07 17:01:38 +1000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+async function renderRoot() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./__mocks__/msw/browser');
+    await worker.start();
+  }
+
+  const RootComponent = (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+
+  const rootElement = document.getElementById('root') as HTMLElement;
+  const root = createRoot(rootElement);
+  root.render(RootComponent);
+}
+
+renderRoot();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
