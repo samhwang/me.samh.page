16af40957e341a8bad5f1a64e5452c08f7e1fc47
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-07 21:29:57 +0700

    Use "rebuild" path only in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isProduction = (mode: string) => mode === 'production';
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/rebuild/',
-});
+  base: isProduction(mode) ? '/rebuild/' : '/',
+}));
