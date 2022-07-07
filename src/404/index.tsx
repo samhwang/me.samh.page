c7774d97f0032d8e37b45931201fefdd7f4395d9
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-07 19:19:43 +0700

    Add resume page and 404 page

---
+++ src/404/index.tsx
@@ -0,0 +1,3 @@
+export default function ErrorPage() {
+  return <>There&apos;s nothing here!</>;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ResumePage from './Resume/page';
+import ErrorPage from './404';
+
 function App() {
-  return <>Barebones App</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ResumePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
