bff2553203c03e694db2eac3d59c4ac4e2c8682d
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-07 19:07:08 +0700

    Clean up the templates

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('renders learn React link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
-});
