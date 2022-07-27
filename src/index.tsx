99ec61a3fd81ebd828c62eb750da14c725c89650
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-27 21:43:49 +1000

    Rename App file to match naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
 async function renderRoot() {
