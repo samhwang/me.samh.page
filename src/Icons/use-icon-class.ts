0483938fde2ae653e61228e9b7c42ba6a009962a
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-27 21:48:18 +1000

    Update useIconClass hook to match new useToggle mantine hook

--- src/Icons/use-icon-class.ts
+++ src/Icons/use-icon-class.ts
@@ -4,9 +4,9 @@ import { useToggle } from '@mantine/hooks';
 export default function useIconClass(name: string) {
   const DEFAULT_ICON_CLASS = useMemo<string>(() => `devicon-${name}`, [name]);
 
-  const [currentIconClass, toggleIconClass] = useToggle(DEFAULT_ICON_CLASS, [
-    `${DEFAULT_ICON_CLASS} colored`,
+  const [currentIconClass, toggleIconClass] = useToggle([
     DEFAULT_ICON_CLASS,
+    `${DEFAULT_ICON_CLASS} colored`,
   ]);
 
   return [currentIconClass, toggleIconClass] as const;
