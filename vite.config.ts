200dbb473891fec7a18b4d6654b3a696230ee330
Sam Huynh <samhwang2112.dev@gmail.com>
2022-09-17 00:27:38 +1000

    Add PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const isProduction = (mode: string) => mode === 'production';
 
@@ -8,8 +9,37 @@ export default defineConfig(({ mode }) => ({
   build: {
     manifest: true,
   },
-  plugins: [react()],
-  base: isProduction(mode) ? '/rebuild/' : '/',
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      base: isProduction(mode) ? '/rebuild/' : '/',
+      includeAssets: ['favicon.svg', 'robots.txt', 'humans.txt'],
+      manifest: {
+        name: 'samhwang page',
+        short_name: 'samhwang',
+        description: 'homepage for samhwang',
+        icons: [
+          {
+            src: 'img192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'img512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'img512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
