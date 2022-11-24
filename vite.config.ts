e9c8bc76721d6d7d896d4ac6b6672662b3697b3e
Sam Huynh <samhwang2112.dev@gmail.com>
2022-11-25 03:54:38 +1100

    Temporarily disable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +8,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    VitePWA(),
   ],
   test: {
     globals: true,
