0357734e073c745b1ba116e3de6e1b4af1166ae5
Sam Huynh <samhwang2112.dev@gmail.com>
2022-12-06 11:03:06 +1100

    Update footer tag line

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
-  const GitHubLink = <Link to="https://github.com/samhwang">Sam Huynh</Link>;
-
-  const GitHubPages = <Link to="https://pages.github.com/">GitHub Pages</Link>;
-
-  const ViteLink = <Link to="https://www.vitejs.dev/">Vite.</Link>;
+  const LINKS = {
+    github: <Link to="https://github.com/samhwang">Sam Huynh</Link>,
+    githubPages: <Link to="https://pages.github.com/">GitHub Pages</Link>,
+    githubAction: <Link to="https://github.com/features/actions">GitHub Actions</Link>,
+    vite: <Link to="https://www.vitejs.dev/">Vite</Link>,
+    react: <Link to="https://reactjs.org/">React</Link>,
+  };
 
   return (
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
       <p>
         {`© ${new Date().getFullYear()} `}
-        {GitHubLink}
-        {`. Hosted on `}
-        {GitHubPages}
-        {`. Built with `}
-        {ViteLink}
+        {LINKS.github}.{` Hosted on `}
+        {LINKS.githubPages}.{` Built with `}
+        {LINKS.vite}
+        {` and `}
+        {LINKS.react}. {` Deployed with `}
+        {LINKS.githubAction}.
       </p>
     </section>
   );
