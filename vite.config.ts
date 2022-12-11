9de91f3851c0e0ee7b357e783bd3130e66bbb9f5
Sam Huynh <samhwang2112.dev@gmail.com>
2022-12-11 11:33:19 +1100

    Update to Vite4 and enable SWC

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import reactBabel from '@vitejs/plugin-react';
+import reactSwc from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 import { manifest } from './metadata/metadata';
+
+const useSwc = true;
+const react = useSwc ? reactSwc : reactBabel;
 
 // https://vitejs.dev/config/
 export default defineConfig({
