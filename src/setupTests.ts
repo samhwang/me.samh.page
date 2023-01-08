ef288b51a00c37c40552acdca2d157dff8ffd8d5
Sam Huynh (he/him) <samhwang2112.dev@gmail.com>
2023-01-08 04:58:16 +0000

    Set system time in tests so it renders the same year

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import { cleanup } from '@testing-library/react';
 import matchers, { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-import { expect, afterEach } from 'vitest';
+import { beforeAll, afterAll, afterEach, expect, vi } from 'vitest';
 
 declare global {
   namespace Vi {
@@ -9,6 +9,16 @@ declare global {
 }
 
 expect.extend(matchers);
+
+beforeAll(() => {
+  vi.useFakeTimers();
+  const currentDate = new Date().setFullYear(2022);
+  vi.setSystemTime(currentDate);
+});
+
+afterAll(() => {
+  vi.useRealTimers();
+});
 
 afterEach(() => {
   cleanup();
