c7125c88d5f960e686759dda041924d43da8b785
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-25 23:35:03 +1100

    feat: remove unused class in 404 page

--- src/404/index.tsx
+++ src/404/index.tsx
@@ -8,11 +8,14 @@ export default function ErrorPage() {
     <>
       <SEO title="Error 404: Page not found" description="Error 404: Page not found" />
       <section className={clsx(classes.section, 'p-3 p-lg-5 d-flex align-items-center')}>
-        <header className="major">
+        <header>
           <h2>Error 404: Page not found.</h2>
           <h3>It seems like you&apos;ve hit a dead end. There&apos;s nothing to see here.</h3>
           <h4>
-            Please go back to the <Link to="/">Home page.</Link>
+            Please go back to the{' '}
+            <Link className={clsx(classes.link)} to="/">
+              Home page.
+            </Link>
           </h4>
         </header>
       </section>
