596d3a5d7129a6e1cf5cec12a6916767ff27caa4
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-26 08:27:50 +1100

    feat: install panda

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import './index.css';
 
 function renderRoot() {
   const RootComponent = (
