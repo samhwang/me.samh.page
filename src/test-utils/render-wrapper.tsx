5546bc96a6979548aa5fc8ae6a530e193d136016
Sam Huynh <samhwang2112.dev@gmail.com>
2024-04-26 10:53:10 +1000

    feat: update render wrapper

--- src/test-utils/render-wrapper.tsx
+++ src/test-utils/render-wrapper.tsx
@@ -1,33 +1,6 @@
-import { Outlet, type RouteComponent, RouterProvider, createMemoryHistory, createRootRoute, createRoute, createRouter } from '@tanstack/react-router';
-import type { ReactNode } from 'react';
+import { RouterProvider } from '@tanstack/react-router';
+import { appRouter } from '../router';
 
-type TestWrapperProps = {
-  children: ReactNode;
-};
-
-function createTestRouter(component: RouteComponent) {
-  const rootRoute = createRootRoute({
-    component: () => <Outlet />,
-  });
-
-  const indexRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: '/',
-    component,
-  });
-
-  const routeTree = rootRoute.addChildren([indexRoute]);
-
-  const router = createRouter({
-    routeTree,
-    history: createMemoryHistory(),
-  });
-
-  return router;
-}
-
-export default function RenderWrapper({ children }: TestWrapperProps) {
-  const router = createTestRouter(() => children);
-
-  return <RouterProvider router={router} />;
+export default function RenderWrapper() {
+  return <RouterProvider router={appRouter} />;
 }
