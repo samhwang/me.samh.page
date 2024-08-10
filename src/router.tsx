5f6c893f0705178887b8bb784a07dfe5948ce629
Sam Huynh <samhwang2112.dev@gmail.com>
2024-08-10 20:34:30 +1000

    chore: rename route-tree for consistency

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
+import { routeTree } from './route-tree.gen';
 
 export const appRouter = createRouter({
   routeTree,
