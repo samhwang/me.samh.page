a5d366c151cfc5a57d87f9e0b3f7f79f280fb15b
Sam Huynh <samhwang2112.dev@gmail.com>
2024-08-12 10:06:26 +1000

    test: Add test for about section and not rely just on snapshot

--- src/resume/about.test.tsx
+++ src/resume/about.test.tsx
@@ -1,10 +1,32 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
+import { aboutData as mockAboutData } from '../test-utils/mock-metadata';
 import About from './about';
 
 describe('Render About Data', () => {
   it('Should match snapshot', () => {
     const tree = render(<About />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Should contain correct About content', () => {
+    const tree = render(<About />);
+
+    const firstName = tree.getByText(mockAboutData.firstName);
+    expect(firstName).toBeInTheDocument();
+
+    const lastName = tree.getByText(mockAboutData.lastName);
+    expect(lastName).toBeInTheDocument();
+
+    const address = tree.getByText(mockAboutData.address);
+    expect(address).toBeInTheDocument();
+
+    const link = mockAboutData.socialLinks[0];
+
+    const linkIcon = tree.getByTestId(`social-icon-${link.icon}`);
+    expect(linkIcon).toBeInTheDocument();
+
+    const linkAnchor = linkIcon.parentElement as HTMLAnchorElement;
+    expect(linkAnchor).toBeInTheDocument();
   });
 });
