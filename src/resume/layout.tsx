0968a574f658b35291e2ba9502ccfbbe8dc7eb5f
Sam Huynh <samhwang2112.dev@gmail.com>
2024-08-14 19:22:42 +1000

    feat: move all css imports into index.css

--- src/resume/layout.tsx
+++ src/resume/layout.tsx
@@ -1,10 +1,5 @@
-import '@fontsource/mulish/index.css';
-import '@fontsource/roboto/index.css';
-import '@fontsource/saira-extra-condensed/index.css';
 import { Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'devicon/devicon.min.css';
 import Footer from './footer';
 import Sidebar from './sidebar';
 
