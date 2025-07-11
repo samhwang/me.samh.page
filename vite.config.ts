f893bad65fa4b8226ca6cd674271ea156ca15a69
Sam Huynh <samhwang2112.dev@gmail.com>
2025-07-12 09:47:12 +1000

    feat: split vite and vitest config to minimize discrepancy between vite and vitest config api

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="vitest" />
-
 import { tanstackRouter } from '@tanstack/router-plugin/esbuild';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -23,18 +21,4 @@ export default defineConfig({
       manifest,
     }),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/setup-tests.ts'],
-    coverage: {
-      provider: 'v8',
-      enabled: !!process.env.CI,
-      exclude: ['styled-system', '**/*.config.*', '**/*.d.ts'],
-      include: ['src/resume/**', 'src/icons/**'],
-    },
-    typecheck: {
-      enabled: true,
-    },
-  },
 });
