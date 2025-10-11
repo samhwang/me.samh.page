6447c65ed2f6a5f840d5228026651c6165274e72
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-11 22:49:33 +1100

    feat: remove useMemo and useCallback

--- src/icons/use-icon-class.ts
+++ src/icons/use-icon-class.ts
@@ -1,14 +1,11 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
 import { cx } from '../../styled-system/css';
 
 export default function useIconClass(name: string) {
-  const DEFAULT_ICON_CLASS = useMemo<string>(() => `devicon-${name}`, [name]);
+  const DEFAULT_ICON_CLASS = `devicon-${name}`;
   const [iconClass, setIconClass] = useState(DEFAULT_ICON_CLASS);
 
-  const toggleColorIcon = useCallback(
-    () => setIconClass((currentClass) => cx(DEFAULT_ICON_CLASS, currentClass === DEFAULT_ICON_CLASS && 'colored')),
-    [DEFAULT_ICON_CLASS]
-  );
+  const toggleColorIcon = () => setIconClass((currentClass) => cx(DEFAULT_ICON_CLASS, currentClass === DEFAULT_ICON_CLASS && 'colored'));
 
   return [iconClass, toggleColorIcon] as const;
 }
