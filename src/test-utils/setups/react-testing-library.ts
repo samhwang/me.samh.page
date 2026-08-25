f0d09e9ae5b68585d9d88f8bf0e35ae77f4d73b1
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-25 13:09:44 +1000

    test: mock tanstack devtools in test

--- src/test-utils/setups/react-testing-library.ts
+++ src/test-utils/setups/react-testing-library.ts
@@ -1,5 +1,9 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
+
+vi.mock('@tanstack/react-devtools', () => ({
+  TanStackDevtools: () => null,
+}));
 
 afterEach(() => {
   cleanup();
